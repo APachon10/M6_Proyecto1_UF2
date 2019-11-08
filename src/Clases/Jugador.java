f0e327f211f66ca4e7874e4dd0b4d9c590276b45
Alberto <Pachondestroy95@gmail.com>
2019-11-08 19:57:35 +0100

    Ultimo COmmit en clase, ningun cambio importante

--- src/Clases/Jugador.java
+++ src/Clases/Jugador.java
@@ -1,5 +1,13 @@
 package Clases;
 
 public class Jugador {
-
+	//Parametros
+	int id = 0;
+	String nombre,apellido,apellido2;
+	int goles;
+	int paradas;
+	int asistencias;
+	
+	
+	//Constructor para J
 }
