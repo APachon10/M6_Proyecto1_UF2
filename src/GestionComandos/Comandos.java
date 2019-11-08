cc37a16cf1b4afc051754de4ef5652987b1b1f14
Alberto <Pachondestroy95@gmail.com>
2019-11-08 19:25:57 +0100

    Build Path Hecho

--- src/GestionComandos/Comandos.java
+++ src/GestionComandos/Comandos.java
@@ -1,5 +1,24 @@
 package GestionComandos;
 
-public class Comandos {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import Interfaces.ParametrosConexion;
 
+public class Comandos implements ParametrosConexion{
+	public Connection conexion (String url,String user,String password) {
+		Connection conn = null;
+		
+		try {
+			conn = DriverManager.getConnection(ParametrosConexion.url,ParametrosConexion.user,ParametrosConexion.pass);
+			System.out.println("Conectado con exito ");
+		} catch (Exception e) {
+			System.out.println("Error ");
+			System.out.println("==========");
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	public void crearTabla() {
+		
+	}
 }
---
+++ src/Interfaces/ParametrosConexion.java
@@ -0,0 +1,7 @@
+package Interfaces;
+
+public interface ParametrosConexion {
+	String url = "jdbc:mysql://localhost:3306/footballmanager";
+	String user = "root";
+	String pass = "";
+}
