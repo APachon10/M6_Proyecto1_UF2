4f2523dfbae679cae57425a84dd73e54a317dd50
Alberto <Pachondestroy95@gmail.com>
2019-11-27 17:57:14 +0100

    Procedure para crear Tablas Creado

--- Procedures.sql
+++ Procedures.sql
@@ -5,17 +5,15 @@ CREATE OR REPLACE PROCEDURE GenerarTablas() AS
         CREATE TABLE IF NOT EXISTS Teams(
             ID_equip integer PRIMARY KEY AUTO_INCREMENT,
             nom_equip VARCHAR(20)
-        )
-        
+        );   
         CREATE TABLE IF NOT EXISTS Players(
             ID_jugador INTEGER PRIMARY KEY AUTO_INCREMENT,
             nom_jugador VARCHAR(20),
-			posicio VARCHAR(20)
-            ID_equip INTEGER
+			posicio VARCHAR(20),
+            ID_equip INTEGER,
             nom_equip VARCHAR(20),
             FOREIGN KEY (ID_equip) REFERENCES TEAMS (ID_equip)
         );
-            
         CREATE TABLE IF NOT EXISTS Classification(
             position_id INTEGER PRIMARY KEY AUTO_INCREMENT,
             team VARCHAR(20),
@@ -23,8 +21,7 @@ CREATE OR REPLACE PROCEDURE GenerarTablas() AS
             loses INTEGER,
             drawn INTEGER,
             points INTEGER 
-        );
-        
+        );       
         CREATE TABLE IF NOT EXISTS Matchs(
             match_id INTEGER PRIMARY KEY AUTO_INCREMENT,
             teamA VARCHAR(20),
